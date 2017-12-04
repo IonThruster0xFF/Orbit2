0e6bce3358ca92af89fe735abf3613fe761c1cc5
Guochu Sang <sgc98@sina.com>
2017-12-04 02:42:14 -0800

    增加了星球海拔与水位贴图的shader
    具体样例在Assets/Scene/shaderTest文件夹内

---
+++ Orbit Ⅱ/Assets/Scripts/Physics/Gravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Gravity : MonoBehaviour {
+	private void Awake()
+	{
+		Rigidbody2D rig2d = GetComponent<Rigidbody2D>();
+		rig2d.gravityScale = 0;
+		PhysicsManager Physics = GameObject.Find("PhysicsManager").GetComponent<PhysicsManager>();
+		if (Physics != null)
+		{
+			Physics.mCelestialRequireGravityRigidbody2D.Add(rig2d);
+		}
+	}
+}
---
+++ Orbit Ⅱ/Assets/Scripts/Physics/PhysicsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsManager : MonoBehaviour
+{
+	public List<Rigidbody2D> mCelestialRequireGravityRigidbody2D=new List<Rigidbody2D>();
+	public float mGravityConstant = GetGravityConstant();
+	/// <summary>
+	/// 返回引力常数，待补
+	/// </summary>
+	/// <returns></returns>
+	public static float GetGravityConstant()
+	{
+		return 6.67f;
+	}
+	public void Update()
+	{
+		foreach (var rig2d in mCelestialRequireGravityRigidbody2D)
+		{
+			foreach (var otherRig2d in mCelestialRequireGravityRigidbody2D)
+			{
+				if (rig2d != otherRig2d)
+				{
+					Vector2 dir = (otherRig2d.transform.position - rig2d.transform.position).normalized;
+					float dis = Vector2.Distance(otherRig2d.transform.position, rig2d.transform.position);
+					float gravityValue = rig2d.mass * otherRig2d.mass * mGravityConstant * (1 / (dis * dis));
+					rig2d.AddForce(dir * gravityValue);
+				}
+			}
+		}
+	}
+}
